4622a27b1c6085c3c9cb3f4de9c0e0a0a1c905d1
Preston <mprestonsparks@gmail.com>
2019-03-10 12:28:20 -0500

    added base firebase and momenjs JS code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,57 @@
+
+
+// ****** FIREBASE CODE ***********
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDAERu2OhTJmiBgZIAnXk7lHWAaSAY28Zk",
+    authDomain: "trainscheduler-1c731.firebaseapp.com",
+    databaseURL: "https://trainscheduler-1c731.firebaseio.com",
+    projectId: "trainscheduler-1c731",
+    storageBucket: "trainscheduler-1c731.appspot.com",
+    messagingSenderId: "130724070717"
+};
+firebase.initializeApp(config);
+
+$("#submit-button").on("click", function(e) {
+    e.preventDefault();
+    var name = $("#employee-name").val();
+    var role = $("#role").val();
+    var startDate = $("#start-date").val();
+    var monthlyRate = $("#monthly-rate").val();
+  //   console.log(name);
+    firebase.database().ref().push({
+      Name: name,
+      Role: role,
+      StartDate : startDate,
+      // MonthsWorked: monthsWorked,
+      MonthlyRate: monthlyRate,
+      // TotalBilled: totalBilled
+    });
+})
+
+var newName = firebase.database().ref().on("child_added", function(childSnapshot) {
+  var nameToAdd = childSnapshot.val().Name;
+  console.log(nameToAdd);
+  addName((nameToAdd));
+});
+// console.log(newRecord);
+
+// function writeRowToPage() {
+  function addName(name) {
+      var newRow = $("<tr>")
+      $("#table-body").append(newRow);
+      var newName = $("<td>");
+      newName.attr("class","name");
+      // var counter = 0;
+      // newName.attr("id",parseInt(counter)+1);
+      $(newRow).append(newName);
+      $(newName).text(name);
+  }
+
+
+
+  
+// **** MOMENT JS CODE ********
 // Assumptions
 var tFrequency = 17;
 
@@ -28,3 +82,11 @@ console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
 // Next Train
 var nextTrain = moment().add(tMinutesTillTrain, "minutes");
 console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+
+
+
+
+
+// **** FUNCTIONS TO ADD ****
+// function addRow()
+
