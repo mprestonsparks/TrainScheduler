b716d0223be44729edcece154f4de4621108fbac
Preston Sparks <mprestonsparks@gmail.com>
2019-03-11 10:56:47 -0500

    fixed writing 'minutes away' to table

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,4 @@
 
-
 // ****** FIREBASE CODE ***********
 // Initialize Firebase
 var config = {
@@ -12,6 +11,7 @@ var config = {
 };
 firebase.initializeApp(config);
 
+// Explain
 $("#submitButton").on("click", function(e) {
     e.preventDefault();
     var trainName = $("#trainNameInput").val();
@@ -19,7 +19,6 @@ $("#submitButton").on("click", function(e) {
     var firstTrainTime = $("#firstTrainTimeInput").val();
     var frequency = $("#frequencyInput").val();
     var nextArrival = calculateNextArrival(firstTrainTime,frequency);
-    // var diffTime = calculateMinutesAway(firstTrainTime);
     var minutesAway = testcalcMinutesAway(frequency,firstTrainTime);
     firebase.database().ref().push({
       TrainName: trainName,
@@ -28,118 +27,75 @@ $("#submitButton").on("click", function(e) {
       Frequency: frequency,
       NextArrival: nextArrival,
       MinutesAway: minutesAway
-    //   DiffTime: diffTime
-
-    });
-    console.log("trainName....",trainName);
+      });
 })
 
+// Explain
 firebase.database().ref().on("child_added", function(childSnapshot) {
   var trainNameToAdd = childSnapshot.val().TrainName;
   var destinationToAdd = childSnapshot.val().Destination;
   var frequencyToAdd = childSnapshot.val().Frequency;
-  var firstTrainTimeToAdd = childSnapshot.val().FirstTrainTime;
   var nextArrivalToAdd = childSnapshot.val().NextArrival;
-//   var nextArrivalToAdd = childSnapshot.val().NextTrainTime;
-    var minutesAwayToAdd = childSnapshot.val().MinuteAway;
-
-
-  addRow(trainNameToAdd,destinationToAdd,frequencyToAdd,nextArrivalToAdd);
+  var minutesAwayToAdd = childSnapshot.val().MinutesAway;
+  addRow(trainNameToAdd,destinationToAdd,frequencyToAdd,nextArrivalToAdd,minutesAwayToAdd);
 });
-
-// Write table data from Firebase
-  function addRow(newName,newDestination,newFrequency,newNextArrival) {
-      var newRow = $("<tr>")
-      $("#table-body").append(newRow);
-      // Add Train Name to page
-      var trainName = $("<td>");
-      trainName.attr("class","trainName");
-      $(newRow).append(trainName);
-      $(trainName).text(newName);
-      // Add Destination to page
-      var destination = $("<td>");
-      destination.attr("class","destination");
-      $(newRow).append(destination);
-      $(destination).text(newDestination);
-      // Add Frequency to page
-      var frequency = $("<td>");
-      frequency.attr("class","frequency");
-      $(newRow).append(frequency);
-      $(frequency).text(newFrequency);
-      // Add NextArrival
-      var nextArrival = $("<td>");
-      nextArrival.attr("class","nextArrival");
-      $(newRow).append(nextArrival);
-      $(nextArrival).text(newNextArrival);
-      
-
-  }
-
-
 
 
 // **** MOMENT JS CODE ********
-// Assumptions
 function testcalcMinutesAway(frequency,firstTime){
-
     var tFrequency = parseInt(frequency);
-
-    // Time is 3:30 AM
-    // var firstTime = "03:30";
-
     // First Time (pushed back 1 year to make sure it comes before current time)
     var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
-    console.log(firstTimeConverted);
-
     // Current Time
     var currentTime = moment();
-    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
-
     // Difference between the times
     var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-    console.log("DIFFERENCE IN TIME: " + diffTime);
-
     // Time apart (remainder)
     var tRemainder = diffTime % tFrequency;
-    console.log(tRemainder);
-
     // Minute Until Train
     var tMinutesTillTrain = tFrequency - tRemainder;
-    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
-
     return tMinutesTillTrain;
-
-    // Next Train
-    // var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-    // console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
-
 }
 
-testcalcMinutesAway();
 
+// **** FUNCTIONS ****
+// Write table data from Firebase
+function addRow(newName,newDestination,newFrequency,newNextArrival,newMinutesAway) {
+  var newRow = $("<tr>")
+  $("#table-body").append(newRow);
+  // Add Train Name to page
+  var trainName = $("<td>");
+  trainName.attr("class","trainName");
+  $(newRow).append(trainName);
+  $(trainName).text(newName);
+  // Add Destination to page
+  var destination = $("<td>");
+  destination.attr("class","destination");
+  $(newRow).append(destination);
+  $(destination).text(newDestination);
+  // Add Frequency to page
+  var frequency = $("<td>");
+  frequency.attr("class","frequency");
+  $(newRow).append(frequency);
+  $(frequency).text(newFrequency);
+  // Add NextArrival
+  var nextArrival = $("<td>");
+  nextArrival.attr("class","nextArrival");
+  $(newRow).append(nextArrival);
+  $(nextArrival).text(newNextArrival);
+  // Add MinutesAway
+  var minutesAway = $("<td>");
+  minutesAway.attr("class","minutesAway");
+  $(newRow).append(minutesAway);
+  $(minutesAway).text(newMinutesAway);
+}
 
-
-
-
-// // Next Train
-// var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-// console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
-
-
-
-// **** NEW SHIT ****
+// Explain
 function calculateNextArrival (firstTrainTime,frequency) {
     var firstTimeConverted = moment(firstTrainTime, "HH:mm").subtract(1, "years");
     var nextTime = firstTimeConverted.add(frequency,"m");
     var nextTimeConverted = moment(nextTime, "HH:mm");
-    return nextTimeConverted.format("HH:mm A"); // ****** DOESN'T CONVER FROM 24-HR to AM/PM, DOCS SAID ADD a OR p AT END OF STRING
-    // http://momentjs.com/docs/#/parsing/string-format/
+    return nextTimeConverted.format("HH:mm A");
 }
 
-// function calculateMinutesAway(firstTime) {
-//     // var currentTime = moment();
-//     var diffTime = moment().diff(moment(firstTime), "minutes");
-//     return diffTime;
-
-// }
 
